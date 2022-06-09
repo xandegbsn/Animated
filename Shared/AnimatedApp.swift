6c6c54ded71c8263325d4f6108d8a03821d9c892
Xande Gibson <alegbsn@gmail.com>
2022-06-09 10:01:20 -0300

    Initial Commit

---
+++ Shared/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Shared
+//
+//  Created by Alexandre Gibson on 09/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
