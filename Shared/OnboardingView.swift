fb07b79a14008429f6bc86d9736a783d70233e0d
Xande Gibson <alegbsn@gmail.com>
2022-06-09 10:37:30 -0300

    Onboarding Animantion

---
+++ Shared/OnboardingView.swift
@@ -0,0 +1,28 @@
+//
+//  OnboardingView.swift
+//  Animated
+//
+//  Created by Alexandre Gibson on 09/06/22.
+//
+
+import SwiftUI
+import RiveRuntime
+
+struct OnboardingView: View {
+    var body: some View {
+        RiveViewModel(fileName: "shapes").view()
+            .ignoresSafeArea()
+            .blur(radius: 30)
+            .background(
+                Image("Spline")
+                    .blur(radius: 50)
+                    .offset(x: 200, y: 100)
+            )
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
